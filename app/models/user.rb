40e7fec4564680156050550037c39ecc87611ef8
MIzoguchi Ririka <14dm160h@rikkyo.ac.jp>
2017-12-22 00:34:34 +0900

    userモデル設定変更

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,12 @@
 class User < ActiveRecord::Base
+
+  validates :email, presence: true
+  validates :nickname, presence: true
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :trackable, :validatable
+
+  has_many :posts
+  has_many :comments
+
 end
+
