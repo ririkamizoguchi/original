9927265632e9601afcf10afbaa46a2e474d683eb
MIzoguchi Ririka <14dm160h@rikkyo.ac.jp>
2017-12-22 00:15:38 +0900

    ルーティング設定変更

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,23 @@
 Rails.application.routes.draw do
-  resources :users, only: [:show]
+
+  resources :users
+    get 'users' => 'users#index'
+
   resources :posts
+    get   'posts'      =>  'posts#index'
+    get   'posts/new'  =>  'posts#new'
+
   resources :comments
+
   resources :bookmarks
   resources :genre_places
   resources :places
   resources :prefectures
   resources :situations
   resources :original
+    get 'original' => 'original#index'
+
+
 end
+
+
