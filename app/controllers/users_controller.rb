e4194958a0952bc674ce20552ad812657ba4adaf
MIzoguchi Ririka <14dm160h@rikkyo.ac.jp>
2017-12-26 18:32:16 +0900

    userコントローラー変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -5,6 +5,7 @@ class UsersController < ApplicationController
   end
 
   def show
+    @user = User.find(params[:id])
   end
 
   def new
@@ -12,6 +13,7 @@ class UsersController < ApplicationController
   end
 
   def edit
+    @user = User.find(params[:id])
   end
 
   def create
@@ -21,11 +23,17 @@ class UsersController < ApplicationController
   end
 
   def update
+   @user = User.find(params[:id])
+   if @user.update(user_params)
+     redirect_to user_show_path(@user)
+    else
+      render :edit
+    end
   end
 
   def destroy
   end
   def user_params
-    params.require(:user).permit(:name, :gender, :birthday, :hometown)
+    params.require(:user).permit(:name, :gender, :birthday, :hometown :image)
   end
 end
