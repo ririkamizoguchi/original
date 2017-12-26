45aba4f6b8cbc3253fadff4e29d33d1f6f6dae6e
MIzoguchi Ririka <14dm160h@rikkyo.ac.jp>
2017-12-26 17:05:13 +0900

    カラム修正

--- db/migrate/20171115081254_create_bookmarks.rb
+++ db/migrate/20171115081254_create_bookmarks.rb
@@ -2,8 +2,8 @@ class CreateBookmarks < ActiveRecord::Migration
   def change
     create_table :bookmarks do |t|
 
-      t.reference :post_id
-      t.reference :user_id
+      t.references :post
+      t.references :user
       t.timestamps
     end
   end
