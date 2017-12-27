0d115ad8243a3062b54a8e3a24037791cb9b8811
MIzoguchi Ririka <14dm160h@rikkyo.ac.jp>
2017-12-27 20:17:25 +0900

    usersコントローラー変更

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -6,6 +6,8 @@ class UsersController < ApplicationController
 
   def show
     @user = User.find(params[:id])
+    @nickname = user.nickname
+    @posts = user.posts.page(params[:page]).per(5).order("created_at DESC")
   end
 
   def new
