1f7f3bdfd5e531e5ceef9cf07e039595ce031648
MIzoguchi Ririka <14dm160h@rikkyo.ac.jp>
2017-12-27 20:23:06 +0900

    ニックネームカラム追加

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171226091704) do
+ActiveRecord::Schema.define(version: 20171227105847) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "post_id",    limit: 4
@@ -76,6 +76,7 @@ ActiveRecord::Schema.define(version: 20171226091704) do
     t.datetime "created_at",                                      null: false
     t.datetime "updated_at",                                      null: false
     t.string   "image",                  limit: 255
+    t.string   "nickname",               limit: 255
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
