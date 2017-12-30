5d47ad146213803794a6fcfdea645b056c0308c2
MIzoguchi Ririka <14dm160h@rikkyo.ac.jp>
2017-12-30 22:13:43 +0900

    postsコントローラー変更

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -4,12 +4,15 @@ class PostsController < ApplicationController
   end
 
   def create
+    Post.create(post_params)
+    @post = current_user.posts.last
   end
 
   def new
   end
 
   def edit
+    @post = Post.find(params[:id])
   end
 
   def show
@@ -25,7 +28,14 @@ class PostsController < ApplicationController
 
   def search
     @posts = Post.where('text LIKE ?', "%#{params[:keyword]}%")
+
   end
+
+ private
+
+def post_params
+  params.require(:post).permit(:text).merge(user_id: current_user.id)
+end
 
 
 
