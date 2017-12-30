854cb7baa85a4687cb04fa75809bfca55af05144
MIzoguchi Ririka <14dm160h@rikkyo.ac.jp>
2017-12-30 22:17:15 +0900

    api記載

--- app/controllers/original_controller.rb
+++ app/controllers/original_controller.rb
@@ -1,33 +1,77 @@
 class OriginalController < ApplicationController
-
+require 'net/http'
+require 'uri'
+require 'rexml/document'
 
 def index
 
+
+    @post = Post.new
+    # @situations = Situation.all
+
+    @places = []
+
+    @prefectures = [] # プルダウン用の都道府県データを格納
+    @areas = [] # プルダウン用の地域データを格納
+
+    ### 都道府県 ###
+
+    hokkaido # 北海道
+
+    for i in 1..10 do # 北海道と沖縄以外
+      prefecture(i)
+    end
+
+    okinawa # 沖縄
+
+    # p @prefectures
+    p @prefectures
+
+
+    ### 地域 ###
+
+    # area
+
+
+  end
+
+  def hokkaido
+    uri = URI.parse("http://jws.jalan.net/APICommon/AreaSearch/V1/?key=and1609fe48610")
+    xml = Net::HTTP.get(uri)
+    json = Hash.from_xml(xml).to_json
+    result = JSON.parse(json)
+
+    @prefectures << result["Results"]["Area"]["Region"][0]["name"]
+
+  end
+
+  def okinawa
+    uri = URI.parse("http://jws.jalan.net/APICommon/AreaSearch/V1/?key=and1609fe48610")
+    xml = Net::HTTP.get(uri)
+    json = Hash.from_xml(xml).to_json
+    result = JSON.parse(json)
+
+    @prefectures << result["Results"]["Area"]["Region"][11]["name"]
+
+  end
+
+  def prefecture(i)
+    uri = URI.parse("http://jws.jalan.net/APICommon/AreaSearch/V1/?key=and1609fe48610")
+    xml = Net::HTTP.get(uri)
+    json = Hash.from_xml(xml).to_json
+    result = JSON.parse(json)
+
+    result["Results"]["Area"]["Region"][i]["Prefecture"].each_with_index {|pref| @prefectures << pref["name"]}
+
+  end
+
+  def smallarea
+
+
+  end
+
 end
 
-def create
-end
 
-def new
-end
-
-def edit
-end
-
-def show
-end
-
-def update
-end
-
-def destroy
-end
-
-def search
-
-end
-
-
-end
 
 
