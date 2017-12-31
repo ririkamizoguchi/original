df6def389f3e41984a4c1f787eedd014a378e655
MIzoguchi Ririka <14dm160h@rikkyo.ac.jp>
2017-12-31 16:12:04 +0900

    schemaファイル変更点

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20171230100641) do
+ActiveRecord::Schema.define(version: 20171231070418) do
 
   create_table "bookmarks", force: :cascade do |t|
     t.integer  "post_id",    limit: 4
@@ -48,6 +48,7 @@ ActiveRecord::Schema.define(version: 20171230100641) do
     t.string   "prefecture", limit: 255
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "post_id",    limit: 4
   end
 
   create_table "situation_places", force: :cascade do |t|
