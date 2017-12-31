cf3640925d6ea1490b22c957c53fe3febc6acf89
MIzoguchi Ririka <14dm160h@rikkyo.ac.jp>
2017-12-31 16:10:47 +0900

    postモデル追記

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,8 @@
 class Post < ActiveRecord::Base
     belongs_to :user
+    belongs_to :prefecture
     has_many :comments
+    has_many :bookmarks
+    has_many :situaions, through: :posts_situaions
+    has_many :posts_situaions, dependent: :destroy
 end
