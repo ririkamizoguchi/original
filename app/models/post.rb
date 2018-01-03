72685ad6e1ede583a57a8ff01dfda17a9824f842
MIzoguchi Ririka <14dm160h@rikkyo.ac.jp>
2018-01-03 19:01:27 +0900

    post.rb追記

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,6 +1,7 @@
 class Post < ActiveRecord::Base
     belongs_to :user
     belongs_to :prefecture
+    belongs_to :smallarea
     has_many :comments
     has_many :bookmarks
     has_many :situaions, through: :posts_situaions
