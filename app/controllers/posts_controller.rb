bec40ec580931395940567c9b920dc72bd95832a
MIzoguchi Ririka <14dm160h@rikkyo.ac.jp>
2018-01-30 10:39:09 +0900

    change html

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -18,9 +18,24 @@ class PostsController < ApplicationController
   end
 
   def update
+    # Post.update(post_params)
+    @post = Post.find(params[:id])
+    if @post.user_id == current_user.id
+      @post.update(post_params)
+
+   # if @post.update(post_params)
+   #   redirect_to post_show_path(@post)
+   #  else
+   #    render :edit
+    end
+
   end
 
   def destroy
+    post = Post.find(params[:id])
+    if post.user_id == current_user.id
+      post.destroy
+    end
   end
 
   def search
@@ -30,10 +45,16 @@ class PostsController < ApplicationController
 
  private
 
-def post_params
-  binding.pry
-  params.require(:post).permit(:text, tag_lists: []).merge(user_id: current_user.id, prefecture_id: params[:post][:tag_list][0], smallarea_id: params[:post][:tag_list][1], situation_id: params[:post][:tag_list][2])
+  def post_params
+    # binding.pry
+    params.permit(:text)
+
+    # params.require(:post).params.permit(:text).merge(user_id: current_user.id)
+    # params.require(:post).permit(:text, tag_lists: []).merge(user_id: current_user.id, prefecture_id: params[:post][:tag_list][0], smallarea_id: params[:post][:tag_list][1], situation_id: params[:post][:tag_list][2])
+  end
+
 end
 
 
-end
+
+
--- app/models/post.rb
+++ app/models/post.rb
@@ -9,4 +9,6 @@ class Post < ActiveRecord::Base
 
     acts_as_taggable_on :labels
     acts_as_taggable
+
+    # validates :text,    length: { minimum: 5 }
 end
